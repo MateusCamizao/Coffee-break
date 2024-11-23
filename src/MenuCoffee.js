ad58cefee4c6dc2fdf0e66559c1e7fcb365a1f44
MateusCamizao <mt.oliveiracamizao@gmail.com>
2024-11-23 00:22:28 -0300

    ajust menu coffe

--- src/MenuCoffee.js
+++ src/MenuCoffee.js
@@ -1,7 +1,62 @@
+import "./MenuCoffee.css"
+
 function MenuCoffee(){
     return(
-        <div>
-            TESTEE
+        <div class="menu-coffee">
+            
+           <div class="menu-coffee-content">
+           <h1>Cardápio Café Especial</h1>
+            <h2>Coffee Break</h2>
+                <h5>
+                    No Coffee Break, cada xícara é uma celebração do sabor. Oferecemos cafés 100% arábica, provenientes de fazendas parceiras, 
+                    com grãos especiais e de nota superior a 80, cuidadosamente selecionados para garantir a melhor experiência sensorial. 
+                    Deixe-se envolver pelos aromas e sabores únicos de cada blend e transforme sua pausa em um momento inesquecível.
+                </h5>
+           </div>
+           <div class="menu-coffee-grid">
+                <div>
+                <h2>1. Café Espresso da Fazenda</h2>
+                <p>Intensidade: Alta | Perfil: Chocolate amargo, caramelo e notas de nozes</p>
+                <h5>
+                O espresso perfeito para quem aprecia a intensidade dos cafés finos. Feito com grãos arábica de uma das melhores fazendas da região,
+                 traz notas de chocolate amargo, leve dulçor de caramelo e um toque suave de nozes. 
+                Aprecie uma experiência rica e sofisticada, ideal para começar o dia com o pé direito.
+                </h5>
+                </div>
+
+                <div>
+                <h2>2. Café Filtrado Terroir</h2>
+                <p>Intensidade: Média | Perfil: Frutas vermelhas, florais e leve acidez</p>
+                <h5>
+                Uma verdadeira viagem sensorial. O café filtrado é delicado e ao mesmo tempo marcante, 
+                com notas frescas de frutas vermelhas e um leve toque floral. A acidez suave e equilibrada traz uma sensação de frescor, 
+                tornando-o perfeito para ser degustado em momentos de relaxamento e descontração.
+                </h5>
+                </div>
+
+                <div>
+                <h2>3. Café Cold Brew com Toque de Baunilha</h2>
+                <p>Intensidade: Suave | Perfil: Café gelado, baunilha e notas de frutas tropicais</p>
+                <h5>
+                Para quem busca uma experiência refrescante e suave. Nosso Cold Brew é extraído lentamente, com grãos arábica de alta qualidade, 
+                e é complementado por um toque sutil de baunilha e frutas tropicais. 
+                Ideal para um momento de pausa relaxante, especialmente nos dias mais quentes, quando a leveza do café é a protagonista.
+                </h5>
+                </div>
+
+                <div>
+                <h2>4. Café Latte da Tradição</h2>
+                <p>Intensidade: Média | Perfil: Creme de leite, cacau e leve doçura</p>
+                <h5>
+                Para aqueles que amam a suavidade e o equilíbrio. Nosso Café Latte é preparado com leite vaporizado e um shot de espresso, 
+                criando uma combinação cremosa e envolvente.
+                O sabor do café é suavizado pelo toque aveludado do leite, 
+                enquanto uma leve doçura de cacau finaliza a experiência com suavidade e harmonia.
+                </h5>
+                </div>
+
+                <p>Coffee Break – Sua Pausa, Nossa Experiência. Aproveite o momento, desfrute do sabor e descubra o prazer de um café realmente especial.</p>
+           </div>
         </div>
     );
 }
